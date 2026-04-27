0bb3c1d9f7e94141bbf9ab55eebb311fca86ec95
TrueModder <rootacessbyuncogn@gmail.com>
2026-04-27 10:37:41 -0300

    Added some files

---
+++ lineage_tundra.mk
@@ -0,0 +1,28 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from tundra device
+$(call inherit-product, device/motorola/tundra/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_tundra
+PRODUCT_DEVICE := tundra
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := motorola edge 30 fusion
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="tundra_g-user 14 U1SJS34.2-92-10-9 40971b-55161 release-keys" \
+    BuildFingerprint=motorola/tundra_g/tundra:14/U1SJS34.2-92-10-9/40971b-55161:user/release-keys \
+    DeviceProduct=tundra_g
